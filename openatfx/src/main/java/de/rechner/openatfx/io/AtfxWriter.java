7dc1e0e1f8f124be4f0e57b33e608a3e4d56be12
crechner <crechner@3fec9929-6ec6-4949-bc57-c3a92d7ac694>
2011-11-30 19:55:32 +0000

    git-svn-id: https://svn.code.sf.net/p/openatfx/code/trunk@75 3fec9929-6ec6-4949-bc57-c3a92d7ac694

--- openatfx/src/main/java/de/rechner/openatfx/io/AtfxWriter.java
+++ openatfx/src/main/java/de/rechner/openatfx/io/AtfxWriter.java
@@ -191,7 +191,7 @@ public class AtfxWriter {
         writeElement(streamWriter, AtfxTagConstants.EXPORTED_BY, "openATFX");
         writeElement(streamWriter, AtfxTagConstants.EXPORTER, "openATFX");
         writeElement(streamWriter, AtfxTagConstants.EXPORT_DATETIME, ODSHelper.getCurrentODSDate());
-        writeElement(streamWriter, AtfxTagConstants.EXPORTER_VERSION, "0.1.4");
+        writeElement(streamWriter, AtfxTagConstants.EXPORTER_VERSION, "0.1.7");
         streamWriter.writeEndElement();
     }
 
